217011349d385a1687b5857a6d0b6f13535bea10
hasilm <58154546+hasilm@users.noreply.github.com>
2026-06-09 16:11:14 -0400

    Update prep.py

--- FEarthquake_mydata/model_building/prep.py
+++ FEarthquake_mydata/model_building/prep.py
@@ -21,7 +21,7 @@ Folder_name="FEarthquake_mydata"
 HF_username="hasilm1"
 App_name="Earthquake_prediction"
 Column_name="latitude"
-Column_name2="longitude
+Column_name2="longitude"
 Test_size=0.2
 Random_state=42
 
