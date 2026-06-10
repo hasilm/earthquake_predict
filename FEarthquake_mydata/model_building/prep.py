04a72f2b3b43b542b245bc82125954fa2960aaf8
hasilm <58154546+hasilm@users.noreply.github.com>
2026-06-09 23:05:56 -0400

    Update prep.py

--- FEarthquake_mydata/model_building/prep.py
+++ FEarthquake_mydata/model_building/prep.py
@@ -30,6 +30,8 @@ target_col = str(Column_name)
 
 # Split into X (features) and y (target)
 X = df.drop(columns=[target_col,'id','time'])
+#X = X.dropna()
+
 y = df[target_col]
 
 # Perform train-test split
