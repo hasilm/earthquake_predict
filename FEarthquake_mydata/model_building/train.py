7982741f684d72496e78ef0ad1719f479e30e683
hasilm <58154546+hasilm@users.noreply.github.com>
2026-06-12 22:38:54 -0400

    Update train.py

--- FEarthquake_mydata/model_building/train.py
+++ FEarthquake_mydata/model_building/train.py
@@ -20,7 +20,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-  
+from sklearn.model_selection import RandomizedSearchCV
+
 api = HfApi()
 
 HF_username="hasilm1"
